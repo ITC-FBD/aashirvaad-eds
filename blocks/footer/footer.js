e0fa267d543c855eca9a106cbe39370a8a41af78
VandanaBV <vandana.bv@axeno.co>
2024-05-15 16:00:53 +0530

    added aashirvaad campaign

---
+++ blocks/footer/footer.js
@@ -0,0 +1,23 @@
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
+
+/**
+ * loads and decorates the footer
+ * @param {Element} block The footer block element
+ */
+export default async function decorate(block) {
+  // load footer as fragment
+  const footerMeta = getMetadata('footer');
+  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  const fragment = await loadFragment(footerPath);
+
+  // decorate footer DOM
+  block.textContent = '';
+  const footer = document.createElement('div');
+  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  block.append(footer);
+  block.querySelector('.icon-like').addEventListener('click', () => {
+    const imgElement = document.querySelector('.icon-like img');
+    imgElement.src = `${window.hlx.codeBasePath}/icons/red-like.svg`;
+  });
+}
---
+++ scripts/delayed.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/no-cycle
+import { sampleRUM } from './aem.js';
+
+// Core Web Vitals RUM collection
+sampleRUM('cwv');
+
+// add more delayed functionality here
+const icons = document.querySelector('main').querySelectorAll('span.icon');
+icons.forEach((span) => {
+  const iconName = Array.from(span.classList).find((c) => c.startsWith('icon-')).substring(5);
+  const rating = span.parentElement.parentElement.nextElementSibling.innerText;
+  if (iconName === 'star-rating') {
+    span.style = `--actual-rating: ${rating};`;
+  }
+});
